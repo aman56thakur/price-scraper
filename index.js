e0b97d45ad30050173db141c8b21c18eea0e3bd7
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-09-13 02:30:45 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+const $ = require('cheerio')
+const CronJob = require('cron').CronJob
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const url =
+  'https://www.amazon.in/Acer-ED322QR-31-5-inch-Monitor-Speakers/dp/B07XYD79KR'
+
+async function configureBrowser() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  return page
+}
+
+async function checkPrice(page) {
+  await page.reload()
+  const html = await page.evaluate(() => document.body.innerHTML)
+  $('#priceblock_ourprice', html).each(function () {
+    const rupeePrice = $(this).text()
+    const price = Number(rupeePrice.replace(/[^0-9.-]+/g, ''))
+    if (price <= 150000) sendWelcomeEmail(rupeePrice)
+  })
+}
+
+function sendWelcomeEmail(price) {
+  sgMail.send({
+    to: 'aman56thakur@gmail.com',
+    from: 'amant9656@gmail.com',
+    subject: `Price Dropped to ${price}`,
+    html:
+      '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Price Dropped!!</title></head><body><h1>Price Dropped!!</h1><h2>Buy the Acer Monitor now.</h2><a href="https://www.amazon.in/Acer-ED322QR-31-5-inch-Monitor-Speakers/dp/B07XYD79KR" style="font-size:large">Product Link</a></body></html>',
+  })
+}
+
+async function monitor() {
+  const page = await configureBrowser()
+  let job = new CronJob(
+    '* * */24 * * *',
+    function () {
+      checkPrice(page)
+    },
+    null,
+    true,
+    null,
+    null,
+    true
+  )
+  job.start()
+}
+
+monitor()
